2d95d52e7c4f8e5e33fe05ea06c8000f34e8850f
Raj kumar Maharjan <108932060+RajKumarMaharjan@users.noreply.github.com>
2023-10-06 13:58:11 +0545

    manage nav search bar and dropdown list

--- js/index.js
+++ js/index.js
@@ -1,15 +1,33 @@
 
 const searchInput = document.getElementById("search-box")
-const  isClicked = true;
+const navbarList = document.getElementById("navbar-menu")
+let isClicked = true;
 
 function toggleInput(){
     if(isClicked)
     {
         searchInput.style.display = 'block';
+        navbarList.style.display = 'block';
         isClicked = false;
     }else{
         searchInput.style.display = 'none';
+        navbarList.style.display = 'none';
         isClicked = true;
     }
 };
 
+const dropdownList = document.getElementById("dropdown-menu")
+const navbarLink = document.getElementById("navbar-menu")
+let onClicked = true;
+
+function dropdown(){
+    if(onClicked){
+        dropdownList.style.display = 'block';
+        navbarLink.style.display = 'block';
+        onClicked = false;
+    }else{
+        dropdownList.style.display = 'none';
+        navbarLink.style.display = 'none';
+        onClicked = true;
+    }
+};
