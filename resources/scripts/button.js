31e2e7e01e0be3da29dcdaf0a0c8a787fa35fa7e
Raj kumar Maharjan <108932060+RajKumarMaharjan@users.noreply.github.com>
2023-10-13 19:45:24 +0545

    smooth btn modify

---
+++ resources/scripts/button.js
@@ -0,0 +1,18 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+    const scrollToContactButton = document.getElementById('scrollToContact');
+    const contactUsSection = document.getElementById('contact-us');
+
+    scrollToContactButton.addEventListener('click', function(){
+        contactUsSection.scrollIntoView({behavior: 'smooth'});
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+    const scrollToCResourceButton = document.getElementById('scrollToResources');
+    const resourceSection = document.getElementById('resources');
+
+    scrollToCResourceButton.addEventListener('click', function(){
+        resourceSection.scrollIntoView({behavior: 'smooth'});
+    });
+});
